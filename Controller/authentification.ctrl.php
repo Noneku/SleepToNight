808877b9e8605fac31459fa70c6dad7389d99ce9
59013-96-05 <59013-96-05@exchange.ad.afpanet>
2023-07-03 14:05:32 +0200

    fonction connexion

--- Controller/authentification.ctrl.php
+++ Controller/authentification.ctrl.php
@@ -8,17 +8,16 @@ if(isset($_POST['submit']))
     $login=$_POST['nom_utilisateur'];
     $password=$_POST['password'];
     $db = new DBManager();
-    $sql="SELECT * FROM utilisateur where nom_utilisateur='$login'";
+    $sql="SELECT * FROM utilisateur where nom_utilisateur='$login'and mot_de_passe='$password'";
     $result= $db->getBdd()->prepare($sql);
     $result->execute();
     if($result->rowCount()>0)
 
     {
         $data = $result->fetchAll();
-        if (password_verify($password,$data[0]['mot_de_passe'])){
             $_SESSION['nom_utilisateur'] = $login;
             echo "Connexion effectuée";
-}
+
     }else{
         echo "vous n'êtes pas inscrit";
     }
