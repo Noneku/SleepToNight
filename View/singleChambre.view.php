5bd63dcaf7a1970bd313d311657ad2c10a658929
None.ku <dev.nassim.pro@gmail.com>
2023-07-05 14:37:22 +0200

    Feature singleChambre

--- View/singleChambre.view.php
+++ View/singleChambre.view.php
@@ -10,13 +10,11 @@
 </head>
 
 <body>
-    <?php include('../Model/DBManager.class.php');
-    $db = new DBManager();
-    $listChambres = $db->selectListeChambre();
-    $chambre = $db->selectById("chambre", "id_chambre", $_POST['id_chambre']);
-
-    var_dump($chambre);
-
+    <?php
+        include('../Model/DBManager.class.php');
+        $db = new DBManager();
+        $chambre = $db->selectById("chambre", "id_chambre", $_POST['id_chambre']);
+        $category = $db->selectById("categorie", "id_categorie", $chambre[0]['id_categorie']);
     ?>
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -42,7 +40,7 @@
                     </form>
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="View/connexion.view.php">Connexion</a>
+                            <a class="nav-link active" aria-current="page" href="View/ClientView/connexion.view.php">Connexion</a>
                         </li>
                     </ul>
                 </div>
@@ -54,8 +52,9 @@
             <div class="card" style="width: 18rem;">
                 <img src="https://picsum.photos/200" class="card-img-top" alt="">
                 <div class="card-body">
-                    <h5 class="card-title">Chambre numéro : <?php echo $chambre[0]['id_chambre'] ?></h5>
-                    <h4 class="card-title">Prix : <?php echo $chambre[0]['prix'] . "€" ?></h5>
+                    <h5 class="card-title">Chambre numéro : <?php echo $chambre[0]['id_chambre']; ?></h5>
+                    <h4 class="card-title">Prix : <?php echo $chambre[0]['prix'] . "€"; ?></h4>
+                    <h4 class="card-title">Categorie : <?php echo $category[0]['designation'];?></h4>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
             </div>
--- index.php
+++ index.php
@@ -7,12 +7,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="stylesheet/styles.css">
     <title>Hotel SleepToNight</title>
-    <!-- Font Awesome -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
-    <!-- MDB -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.css" rel="stylesheet" />
 </head>
 
 <body>
